3116f17ba6ddfb81f0896cd38304b9a5db70f9d4
csaba0411 <csaba0411@gmail.com>
2020-11-10 08:22:30 +0100

    first commit

---
+++ src/test/java/com/codecool/todobackend/TodobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.todobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodobackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
